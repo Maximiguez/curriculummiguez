204684636f27b0a9617464ce5f40854c29d2fcba
Maximiliano <miguezmaximil@gmail.com>
2023-02-28 21:49:20 -0300

    Curriculum vitae miguez

---
+++ cv.js
@@ -0,0 +1,6 @@
+function telefono() {
+    document.getElementById("telefono").innerHTML = "2664746425";
+  }
+  function email() {
+    document.getElementById("email").innerHTML = "miguezmaximil@gmail.com";
+  }
